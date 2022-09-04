e06df218ffe615a63b2caff06fd06750f0f3b637
Sujay U N <sujayun@yahoo.com>
2022-09-04 11:05:06 +0530

    Prod CRUD

--- Sdb/namsdb.sql
+++ Sdb/namsdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 04, 2022 at 07:00 AM
+-- Generation Time: Sep 04, 2022 at 07:34 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -40,34 +40,8 @@ CREATE TABLE `namtbl` (
 --
 
 INSERT INTO `namtbl` (`UidKol`, `TtlKol`, `DtlKol`, `AmtKol`, `TymKol`) VALUES
-(1, 'Apple', '5', 100, '2021-11-18 04:36:58'),
-(2, 'Mango', '15', 100, '2021-11-18 05:48:13'),
-(3, 'Banana', '10', 250, '2021-11-18 08:55:55'),
-(4, 'Orange', '1', 300, '2021-11-18 08:56:07'),
-(5, 'TV', '1', 30000, '2021-11-18 08:57:07'),
-(6, 'Mobile', '1', 30000, '2021-11-18 08:59:46'),
-(7, 'Atta', '5', 500, '2021-11-18 08:59:46'),
-(8, 'Red Chilli', '500', 50, '2021-11-18 08:59:46'),
-(9, 'Green Chilli', '50', 20, '2021-11-18 08:59:46'),
-(10, 'Pepper', '100', 50, '2021-11-18 08:59:46'),
-(11, 'Chat Masala', '10', 150, '2021-11-18 08:59:46'),
-(12, 'PaniPuri', '2', 50, '2021-11-18 08:59:46'),
-(13, 'Masala Puri', '1', 25, '2021-11-18 08:59:46'),
-(14, 'Balepuri', '1', 30, '2021-11-18 08:59:46'),
-(15, 'SavPuri', '2', 50, '2021-11-18 08:59:46'),
-(16, 'Maida', '1', 150, '2021-11-18 09:03:54'),
-(17, 'Rice Floor', '1', 30, '2021-11-18 09:03:54'),
-(18, 'Ragi Floor', '1', 50, '2021-11-18 09:03:54'),
-(19, 'Amul Ice Creem', '500', 250, '2021-11-18 09:03:54'),
-(20, 'Floor Cleaner', '250', 60, '2021-11-18 09:03:54'),
-(21, 'Bread', '1', 30, '2021-11-18 09:03:54'),
-(22, 'Veg Puff', '5', 150, '2021-11-18 09:03:54'),
-(23, 'Egg Puff', '5', 250, '2021-11-18 09:03:54'),
-(24, 'Veg Burgger', '5', 800, '2021-11-18 09:03:54'),
-(25, 'Chicken Burger', '5', 1500, '2021-11-18 09:03:54'),
-(26, 'Tomato', '1', 20, '2021-11-18 11:04:42'),
-(27, 'AC', '1', 80000, '2021-11-18 12:24:18'),
-(28, 'ORANGE', '1', 80, '2021-11-18 13:26:55');
+(1, 'TT Table', 'Supreme Table Tennis', 40000, '2021-11-18 05:48:13'),
+(2, 'Banana', '10', 250, '2021-11-18 08:55:55');
 
 --
 -- Indexes for dumped tables
@@ -87,7 +61,7 @@ ALTER TABLE `namtbl`
 -- AUTO_INCREMENT for table `namtbl`
 --
 ALTER TABLE `namtbl`
-  MODIFY `UidKol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `UidKol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- Server.js
+++ Server.js
@@ -1,8 +1,8 @@
 var SvrMgrVap = require('express')
 var SvrMgrVaj = SvrMgrVap()
-SvrMgrVaj.use(SvrMgrVap.json()) //Doubt
+SvrMgrVaj.use(SvrMgrVap.json())
 
-SvrMgrVaj.listen(8080, function()
+SvrMgrVaj.listen(8000, function()
 {
     console.log("Server Started and Running ...")
 })
@@ -14,4 +14,66 @@ var SdbCncVaj = SdbCncVap.createConnection({
     user: "SdbUsr",
     password: "UsrPwd4Sdb",
     database: "NamSdb"
+})
+
+SdbCncVaj.connect((ErrSdbVar)=>
+{
+    if(ErrSdbVar) throw ErrSdbVar
+    
+    console.log("Sdb Connected")
+})
+
+SvrMgrVaj.get("/prod", (ReqNdsVar, ResNdsVar) =>
+{
+    var SqlQryVar=`SELECT TtlKol, DtlKol, AmtKol from NamTbl`
+    SdbCncVaj.query(SqlQryVar, (ErrSdbVar, ResSdbVar)=>
+    {
+        if(ErrSdbVar) throw ErrSdbVar
+        ResNdsVar.json(ResSdbVar)
+    })
+})
+
+SvrMgrVaj.get("/prod/:UidVak", (ReqNdsVar, ResNdsVar) =>
+{
+    var UidVar = ReqNdsVar.params.UidVak
+    var SqlQryVar=`SELECT TtlKol, DtlKol, AmtKol from NamTbl where UidKol='${UidVar}'`
+    SdbCncVaj.query(SqlQryVar, (ErrSdbVar, ResSdbVar)=>
+    {
+        if(ErrSdbVar) throw ErrSdbVar
+        ResNdsVar.json(ResSdbVar[0])
+    })
+})
+
+SvrMgrVaj.post("/prod", (ReqNdsVar, ResNdsVar) =>
+{
+    var NquPrdVar = ReqNdsVar.body
+    var SqlQryVar=`INSERT INTO NamTbl (TtlKol, DtlKol, AmtKol) VALUES ("${NquPrdVar.TtlVak}","${NquPrdVar.DtlVak}","${NquPrdVar.AmtVak}")`
+    SdbCncVaj.query(SqlQryVar, (ErrSdbVar, ResSdbVar)=>
+    {
+        if(ErrSdbVar) throw ErrSdbVar
+        ResNdsVar.json(ResSdbVar)
+    })
+})
+
+SvrMgrVaj.put("/prod/:UidVak", (ReqNdsVar, ResNdsVar) =>
+{
+    var UidVar = ReqNdsVar.params.UidVak
+    var UquPrdVar = ReqNdsVar.body
+    var SqlQryVar=`UPDATE NamTbl SET TtlKol='${UquPrdVar.TtlVak}', DtlKol='${UquPrdVar.DtlVak}', AmtKol='${UquPrdVar.AmtVak}' WHERE UidKol=${UidVar}`
+    SdbCncVaj.query(SqlQryVar, (ErrSdbVar, ResSdbVar)=>
+    {
+        if(ErrSdbVar) throw ErrSdbVar
+        ResNdsVar.json(ResSdbVar)
+    })
+})
+
+SvrMgrVaj.delete("/prod/:UidVak", (ReqNdsVar, ResNdsVar) =>
+{
+    var UidVar = ReqNdsVar.params.UidVak
+    var SqlQryVar=`DELETE FROM NamTbl WHERE UidKol=${UidVar}`
+    SdbCncVaj.query(SqlQryVar, (ErrSdbVar, ResSdbVar)=>
+    {
+        if(ErrSdbVar) throw ErrSdbVar
+        ResNdsVar.json(ResSdbVar)
+    })
 })
